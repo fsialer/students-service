f0544aab5b1e80509a08915f53d3fcb6b0aef794
Fernando <asialer05@hotmail.com>
2024-09-17 22:36:27 -0500

    Estructura base del ms

---
+++ src/test/java/com/tecylab/ms/students/app/students_service/StudentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecylab.ms.students.app.students_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
