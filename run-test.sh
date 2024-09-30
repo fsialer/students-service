b2843f349d98b5927732b55e55836141c2f154e4
Fernando <asialer05@hotmail.com>
2024-09-30 15:33:39 -0500

    Ajuste en main.yml y run-test

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 curl -X POST http://localhost:8086/students \
      -H "Content-Type: application/json" \
-     -d '{"firstname":"Fernando", "lastname":"Sialer","age":33,"":"email":"fernando@gmail.com"}'
+     -d '{"firstName":"Fernando", "lastName":"Sialer","age":33,"email":"fernando@gmail.com","address":"dsd"}'
 
 curl -X GET http://localhost:8086/students \
      -H "Accept: application/json"
