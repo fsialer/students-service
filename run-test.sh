1554e787743fb1ab3df6d91e0046ac627dec9341
Fernando <asialer05@hotmail.com>
2024-09-30 14:52:30 -0500

    Arreglando endpoint de run.test.sh

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
-curl -X POST http://students-service.8086/students \
+curl -X POST http://students-service:8086/students \
      -H "Content-Type: application/json" \
      -d '{"firstname":"Fernando", "lastname":"Sialer","age":33,"":"email":"fernando@gmail.com"}'
 
-curl -X GET http://students-service.8086/students \
+curl -X GET http://students-service:8086/students \
      -H "Accept: application/json"
